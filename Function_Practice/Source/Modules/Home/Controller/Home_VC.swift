47645af3d564f201a2197b58f5d12b440ed6aff7
Nikolai Rivas <nikolai.rivas@gmail.com>
2021-01-18 12:51:32 -0800

    added basic files and folders for app

---
+++ Function_Practice/Source/Modules/Home/Controller/Home_VC.swift
@@ -0,0 +1,26 @@
+//
+//  Home_VC.swift
+//  Function_Practice
+//
+//  Created by Nikolai Nobadi on 1/18/21.
+//
+
+class Home_VC: NiblessVC {
+    
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - VIEWS
+    
+    
+    // MARK: - INITIALIZATION
+    init() {
+        super.init(hasTextFields: true)
+    }
+    
+    
+    // MARK: - LIFE CYCLE
+    override func loadView() {
+        
+    }
+}
---
+++ Function_Practice/Source/Modules/PremiumDetail/Controller/PremiumDetail_VC.swift
@@ -0,0 +1,26 @@
+//
+//  PremiumDetail_VC.swift
+//  Function_Practice
+//
+//  Created by Nikolai Nobadi on 1/18/21.
+//
+
+class PremiumDetail_VC: NiblessVC {
+    
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - VIEWS
+    
+    
+    // MARK: - INITIALIZATION
+    init() {
+        super.init(hasTextFields: true)
+    }
+    
+    
+    // MARK: - LIFE CYCLE
+    override func loadView() {
+        
+    }
+}
