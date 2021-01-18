cfbbef26189c20a94189172678f1d493834656ba
Nikolai Rivas <nikolai.rivas@gmail.com>
2021-01-18 12:36:21 -0800

    Initial Commit

---
+++ Function_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Function_Practice
+//
+//  Created by Nikolai Nobadi on 1/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
