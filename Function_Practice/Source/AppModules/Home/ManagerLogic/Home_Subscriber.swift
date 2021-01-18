fa9d06468e8122ae9d561aa48581cbae1b375ca7
Nikolai Rivas <nikolai.rivas@gmail.com>
2021-01-18 13:27:59 -0800

    added Home_Subscriber

---
+++ Function_Practice/Source/AppModules/Home/ManagerLogic/Home_Subscriber.swift
@@ -0,0 +1,20 @@
+//
+//  Home_Subscriber.swift
+//  Function_Practice
+//
+//  Created by Nikolai Nobadi on 1/18/21.
+//
+
+import Combine
+
+class HomeSubscriber {
+    
+    // MARK: - PROPERTIES
+    private var subscriptions = Set<AnyCancellable>()
+    
+    
+    // MARK: - SUBSCRIPTIONS
+    func subscribe() {
+        
+    }
+}
