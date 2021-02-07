6582287b418e71a319ee87fe3c4da89ac4704dda
Nikolai Rivas <nikolai.rivas@gmail.com>
2021-02-07 13:04:12 -0800

    did stuff

--- Function_Practice/Login/Login_VC.swift
+++ Function_Practice/Login/Login_VC.swift
@@ -9,14 +9,11 @@ import UIKit
 
 class Login_VC: NiblessVC {
     
-    // MARK: - PROPERTIES
-    
-    
     // MARK: - VIEWS
     let titleLabel: BoringLabel = {
         let label = BoringLabel(55, autoSize: true)
         
-        label.text = "Login"
+        label.text = "Sign Up"
         label.textAlignment = .center
         
         return label
@@ -63,7 +60,7 @@ class Login_VC: NiblessVC {
     let loginButton: ShadowButton = {
         let button = ShadowButton()
         
-        button.setTitle("Login", for: .normal)
+        button.setTitle("Sign Up", for: .normal)
         button.addTarget(self, action: #selector(didTapLoginButton(_:)), for: .touchUpInside)
         
         return button
@@ -72,6 +69,7 @@ class Login_VC: NiblessVC {
     let accountButton: UIButton = {
         let button = UIButton(type: .system)
         
+        button.setTitle("Already have an account?", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.addTarget(self, action: #selector(didTapAccountButton(_:)), for: .touchUpInside)
         
@@ -137,11 +135,68 @@ class Login_VC: NiblessVC {
     func didTapLoginButton(_ sender: UIButton) {
         sender.pulse()
         
+        let isSignUp = titleLabel.text == "Sign Up"
+        
+        usernameField.setErrorMessage("")
+        emailField.setErrorMessage("")
+        passwordField.setErrorMessage("")
+        confirmField.setErrorMessage("")
+        
+        guard let username = usernameField.text else { return }
+        guard let email = emailField.text else { return }
+        guard let password = passwordField.text else { return }
+        guard let confirm = confirmField.text else { return }
+        
+        if isSignUp {
+            guard username != "" else { return }
+            
+            guard username.count >= 4 else {
+                usernameField.setErrorMessage("Username too short")
+                return
+            }
+            
+        }
+        
+        guard email != "" else { return }
+        guard email.contains("@") && email.contains(".com") else {
+            emailField.setErrorMessage("Invalid Email")
+            return
+        }
+        
+        guard password != "" else { return }
+        guard password.count >= 6 else {
+            passwordField.setErrorMessage("Password too short")
+            return
+        }
+        
+        if isSignUp {
+            guard confirm == password else {
+                confirmField.setErrorMessage("Passwords don't match")
+                return
+            }
+        }
+        
+        
+        let action = isSignUp ? "signing up" : "logging in"
+        Alert.showAlert(title: "Yay", message: "We're \(action)")
     }
     
     @objc
     func didTapAccountButton(_ sender: UIButton) {
         
+        if titleLabel.text == "Sign Up" {
+            usernameField.alpha = 0
+            confirmField.alpha = 0
+            loginButton.setTitle("Login", for: .normal)
+            titleLabel.text = "Login"
+            accountButton.setTitle("Don't have an account?", for: .normal)
+        } else {
+            usernameField.alpha = 1
+            confirmField.alpha = 1
+            loginButton.setTitle("Sign Up", for: .normal)
+            titleLabel.text = "Sign Up"
+            accountButton.setTitle("Already have an account?", for: .normal)
+        }
     }
 }
 
